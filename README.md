10b3843651ea0f3dbc3cd0b97819ffb0698263a5
Nelson Nobre <technobre@gmail.com>
2025-04-12 14:26:10 +0100

    Kickoff

---
+++ README.md
@@ -0,0 +1,12 @@
+# C# 14 - New Features
+
+Check my post in my [blog](https://nelsonbn.com/blog) where I discuss more about each feature.
+
+## Demos
+
+- [Null-conditional assignment](./src/NullConditionalAssignment/Program.cs)
+
+
+## References
+
+- [.NET Release Notes](https://github.com/dotnet/core/tree/main/release-notes)
---
+++ src/NullConditionalAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿Person? me = new Person { Name = "Nelson" };
+Person? you = null;
+
+me?.Name = "Nelson Nobre"; // This will assign because "me" is not null
+you?.Name = "John Doe"; // This will not assign because "you" is null, but it won't throw an exception
+
+Console.WriteLine($"me: {me?.Name}, you: {you?.Name}"); // Output: me: Nelson Nobre, you: null
+
+class Person
+{
+    public string? Name { get; set; }
+}
